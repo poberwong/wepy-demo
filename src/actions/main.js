00bfbb7ceaf071dcc4ceed37700b4b0f96fad44a
poberWong <poberwong@gmail.com>
2019-02-22 14:58:58 +0800

    完成报告查询列表分页加载、刷新功能

--- src/actions/main.js
+++ src/actions/main.js
@@ -1,6 +1,13 @@
 import config from '../utils/config'
 import request from '../utils/request'
 
-export function getProjects() {
-  return request.get({url: `${config.backend}/projects`})
+export function getProjects () {
+  return request.get({ url: `${config.backend}/projects` })
+}
+
+export function loadReportList (queryObj = {}) {
+  const baseQuery = { offset: 0, limit: 10 }
+  const query = {...baseQuery, ...queryObj}
+  let str = Object.keys(query).map(key => key + '=' + query[key]).join('&')
+  return request.get({ url: config.backend + '/reports?' + str })
 }
